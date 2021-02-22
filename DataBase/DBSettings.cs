f9abc2fbcade4b795490101f69c661cedb0100ad
VadHan <65092760+VadHane@users.noreply.github.com>
2021-02-22 10:56:20 +0200

    Update DBSettings.cs

--- DataBase/DBSettings.cs
+++ DataBase/DBSettings.cs
@@ -4,11 +4,11 @@ namespace TelegramBot.DataBase
 {
     public static class DBSettings
     {
-        public const string Host = "ec2-54-155-208-5.eu-west-1.compute.amazonaws.com";
+        public const string Host = "ec2-54-155-5.eu-west-1.compute.amazonaws.com";
         public const int Port = 5432;
-        public const string Username = "exoobzialqkozf";
+        public const string Username = "exoobzialqkoz";
         public const string Password = "820cf7299fbd1b4bd7b7fb66cbf5d13cb2aceeb048f2c5c5198f16620a12ea72";
-        public const string Datebase = "d6ethqs75s9hh4";
+        public const string Datebase = "d6ethqs75shh4";
         public const bool Pooling = true;
         public const string SSLMode = "Require";
         public const bool TrueServerCertificate = true;
